f1e9db8e4c0db3199c5081fa39110840e8ebbf63
cdosorio <cdosorio@gmail.com>
2018-03-08 21:05:23 -0300

    Agregado ejemplo Lazy Loading

---
+++ Queries/LoadingRelatedObjects/LazyLoading.cs
@@ -0,0 +1,27 @@
+﻿
+using EF6;
+using System;
+using System.Linq;
+
+namespace LoadingRelated
+{
+    /// <summary>
+    /// Problema N+1
+    /// </summary>
+    class LazyLoading
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+                        
+            var courses = context.Courses.ToList();
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine("{0} by {1}", course.Name, course.Author.Name);
+            }
+            Console.ReadKey();
+        }
+                
+    }
+}
