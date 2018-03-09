0cd2f89d382e1c11c8e151851b1711e65cc9118b
carlos osorio <cdosorio@outlook.com>
2018-03-09 17:37:27 -0300

    Clase 64

---
+++ Queries/CRUD/Adding.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6.CRUD
+{
+    class Adding
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var course = new Course
+            {
+                Name = "new course",
+                Description = "new descr",
+                FullPrice = 19.95f,
+                Level = 1,
+                AuthorId = 1
+            };
+
+            context.Courses.Add(course);
+            context.SaveChanges();
+            
+        }
+    }
+}
